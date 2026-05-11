bdaa6fcf0db63fdd1d929ea65b7ba1b6b1acace4
Mrkeit95 <keitdmitrijev95@gmail.com>
2026-05-11 13:37:02 +0800

    Delete app directory

--- app/admin/layout.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@/lib/supabase-server";
-import { redirect } from "next/navigation";
-import AdminNav from "./AdminNav";
-import "../globals.css";
-import "./admin.css";
-
-export default async function AdminLayout({ children }) {
-  return <>{children}</>;
-}
